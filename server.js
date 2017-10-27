e44ca81fc0f102971a3700be858f1d275671cecc
Iryna <iryna.laitaruk@gmail.com>
2017-10-27 22:48:15 +0300

    ange to online-shop

--- server.js
+++ server.js
@@ -1,53 +1,104 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const cors = require('cors');
+//const cors = require('cors');
+const mysql = require('mysql');
 const port = 8000;
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'true'}));
-app.use(cors());
+//app.use(cors());
 
+//MYSQL
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'vivtorok16'
+});
 
-
-  let arr = [{login:"user1",pass:"123"},
-           {login:"user2",pass:"456"},
-           {login:"user3",pass:"789"}];
-
-//app.post('/login', function (req,res) {
-//	
-//	for(var a = 0; a <= arr.length; a++){
-//		
-//	if(req.body.login == arr[a].login){
-//        
-//        
-//		if(req.body.pass == arr[a].pass){
-//            
-//			res.status(200).send("Welcome to site!");
-//            
-//		}else{
-//			res.status(200).send("Wrong password");
-//		}
-//	}else{
-//		res.status(200).send("Wrong login");
-//	}
-//}
-//})
-
+//Авторизація 
 app.post('/login', function (req, res) {
-    if (req.body.login == "admin") {
-        if (req.body.pass == "123") {
-            res.status(200).send("Wellcome to site!");
+    connection.query('SELECT * FROM users  WHERE login = ?', req.body.login, function (err, rows) {
+        if (err) throw err;
+        if (rows[0] != undefined) {
+            if (rows[0].password == req.body.pass) {
+                res.status(200).send("welcome");
+            } else {
+                res.status(200).send("wrong password");
+            }
         } else {
-            res.status(200).send("Wrong password!");
+            res.status(200).send("wrong login");
         }
-    } else {
-        res.status(200).send("Wrong login!");
-    }
-})
+    });
+});
+
+//Реєстрація
+app.post('/login-reg', function (req, res) {
+    connection.query('SELECT * FROM users  WHERE login = ?', req.body.login, function (err, rows) {
+        if (err) throw err;
+        if (rows[0] != undefined) {
+            res.status(200).send("Choose another login!");
+        } else {
+            connection.query('INSERT INTO users SET ?', req.body,
+                function (err, result) {
+                    if (err) throw err;
+                    console.log('user added to database with id: ' + result.insertId);
+
+                }
+
+            );
+//            res.status(200).send(req.body.login+" registered!");
+        }
+    });
+});
+
+//Завантажити дані авторизованого юзера
+app.post('/user-prof', function (req, res) {
+    connection.query('SELECT * FROM users  WHERE login = ?', req.body.login, function (err, rows) {
+        if (err) throw err;
+        if (rows[0] != undefined) {
+             connection.query('SELECT * FROM userpage  WHERE users_id = ?', rows[0].id,
+                function (err, result) {
+                    if (err) throw err;
+                    res.status(200).send(result);
+                }
+            );
+        } else {
+            res.status(200).send("User is undefined");
+        }
+    });
+});
+
+//get items
+app.get('/items', function (req, res) {
+connection.query('SELECT * FROM items', 
+    function (err,rows) {
+        if (err) throw err;
+    console.log('get all itemses,length: ' + rows.length);
+
+res.status(200).send(rows);
+    });
+});
 
 
+
+//
+app.post('/brand-inf', function (req, res) {
+    connection.query('SELECT * FROM brand  WHERE name = ?', req.body.name, function (err, rows) {
+        if (err) throw err;
+        if (rows[0] != undefined) {
+             connection.query('SELECT * FROM items  WHERE brand_id = ?', rows[0].id,
+                function (err, result) {
+                    if (err) throw err;
+                    res.status(200).send(result);
+                }
+            );
+        } else {
+            res.status(200).send("Brand is undefined");
+        }
+    });
 //Усі адреси контролюються клієнтським ангуляром
 app.get('*', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
